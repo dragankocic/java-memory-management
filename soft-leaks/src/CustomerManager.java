054b4d17a46337927cf3712d6e220de1b80d78e1
Dragan Kocic <dragan.kocic@primeholding.com>
2020-06-05 16:42:15 +0200

    Soft-leaks

---
+++ soft-leaks/src/CustomerManager.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class CustomerManager {
+    private List<Customer> customers = new ArrayList<Customer>();
+    private int nextId = 0;
+
+    public  void addCustomer(Customer customer) {
+        synchronized (this) {
+            customer.setId(nextId);
+            nextId++;
+        }
+        customers.add(customer);
+    }
+
+    public Customer getNextCustomer() {
+        Customer result = null;
+        synchronized (this) {
+            if (customers.size() > 0) {
+                result = customers.remove(0);
+            }
+        }
+        //should do:
+        //customers.remove(0);
+        return result;
+    }
+
+    public void howManyCustomers() {
+        int size = 0;
+        size = customers.size();
+        System.out.println("" + new Date() + " : " + size);
+    }
+
+    public void displayCustomers() {
+        synchronized(customers){
+            for (Customer c : customers) {
+                System.out.println(c.toString());
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
